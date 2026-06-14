3de3df560784ec049bc8b4bb66898264f1e775b9
Lucas de Sousa Rosa <roses.lucas404@gmail.com>
2026-06-13 22:54:10 -0300

    feat: add batsim, batsched, evalys, and go to the dev shell
    Merge the experimental-campaigns flake inputs into this repo so
    a single nix develop provides both the notebook environment and
    the campaign runner toolchain.

--- flake.nix
+++ flake.nix
@@ -1,22 +1,36 @@
 {
-  description = "Description for the project";
+  description = "Experimental artifacts and campaign runner";
 
   inputs = {
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    batsim.url = "github:Lucas-Doctorate-Project/batsim";
+    batsched.url = "github:Lucas-Doctorate-Project/batsched";
+    evalys.url = "github:Lucas-Doctorate-Project/evalys";
   };
 
-  outputs = inputs@{ flake-parts, ... }:
+  outputs = inputs@{ flake-parts, batsim, batsched, evalys, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = [ "x86_64-linux" "aarch64-darwin" ];
       perSystem = { config, self', inputs', pkgs, system, ... }:
       let
+        batsim-pkg = inputs.batsim.packages.${system}.default;
+        batsched-pkg = inputs.batsched.packages.${system}.default;
+        evalys-pkg = inputs.evalys.packages.${system}.evalys;
+
         python = pkgs.python3.override {
           self = python;
           packageOverrides = pyfinal: pyprev: {
             entsoe-py = pyfinal.callPackage ./entsoe-py.nix { };
           };
         };
+
+        evalysPython = evalys-pkg.pythonModule;
+        evalysPythonEnv = evalysPython.withPackages (ps: [
+          ps.pandas
+          ps.matplotlib
+          evalys-pkg
+        ]);
       in {
         devShells.default = pkgs.mkShell {
           packages = [
@@ -28,7 +42,13 @@
               python-pkgs.jupyterlab
               python-pkgs.ipykernel
             ]))
+            pkgs.go
+            batsim-pkg
+            batsched-pkg
+            evalysPythonEnv
           ];
+
+          shellHook = "unset GOPATH";
         };
       };
     };
